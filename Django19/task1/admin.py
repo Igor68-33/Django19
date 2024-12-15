e5689870d054cd5f729c65100a1c6c02426a7cc9
User_PC <gid68@mail.ru>
2024-12-15 10:17:11 +0300

    ModelAdmin

--- Django19/task1/admin.py
+++ Django19/task1/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Buyer, Game
+
 
 # Register your models here.
+
+@admin.register(Buyer)
+class Buyer(admin.ModelAdmin):
+    list_display = ('username', 'balance', 'age')
+    search_fields = ('username',)
+    list_filter = ('balance', 'age',)
+    list_per_page = 30
+    readonly_fields = ('balance',)
+
+
+@admin.register(Game)
+class Buyer(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size',)
+    search_fields = ('title',)
+    list_filter = ('cost', 'size',)
+    list_per_page = 20
+
+    # Разбитие по секциям
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'cost', 'size',)
+        }),
+        ('Дополнительные настройки', {
+            'classes': ('collapse',),   # Скрыть дополнительные поля
+            'fields': ('description', 'age_limited',)
+        })
+    )
